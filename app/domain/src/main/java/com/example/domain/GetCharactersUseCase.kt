75815f7ebcd225e89df6a015f8e5db78cf0c7f97
tomekjz <37337857+tomekjz@users.noreply.github.com>
2024-08-13 15:14:21 +0100

    Update GetCharactersUseCase.kt

--- app/domain/src/main/java/com/example/domain/GetCharactersUseCase.kt
+++ app/domain/src/main/java/com/example/domain/GetCharactersUseCase.kt
@@ -2,11 +2,13 @@ package com.example.domain
 
 import com.example.data.Character
 import com.example.data.CharactersRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class GetCharactersUseCase(private val charactersRepository: CharactersRepository) {
 
-    suspend fun getCharacters(): List<Character>? {
-        return charactersRepository.getCharacters()
+    suspend fun getCharacters(): List<Character>? = withContext(Dispatchers.IO) {
+        charactersRepository.getCharacters()
     }
 
 }
